6c497c0fc0540866211a292d144c8e49836423f1
myeonginjin <113578420+myeonginjin@users.noreply.github.com>
2024-07-17 19:38:52 +0900

    upload repo

---
+++ src/main/java/com/study/profanityFilterSystem/ProfanityFilterSystemApplication.java
@@ -0,0 +1,14 @@
+package com.study.profanityFilterSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ProfanityFilterSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfanityFilterSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/study/profanityFilterSystem/config/OpenAiConfig.java
@@ -0,0 +1,4 @@
+package com.study.profanityFilterSystem.config;
+
+public class OpenAiConfig {
+}
